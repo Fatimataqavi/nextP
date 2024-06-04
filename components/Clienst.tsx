8e79b425db4776806b8b69ee86ea117cc6ece1c5
Fatimataqavi <147706423+Fatimataqavi@users.noreply.github.com>
2024-06-04 13:21:50 +0330

    first commit

---
+++ components/Clienst.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
+import { companies } from "@/data";
+
+const Clienst = () => {
+  return (
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind words from {""}
+        <span className="text-purple"> satisfied clients</span>
+      </h1>
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Clienst;
---
+++ data/index.ts
@@ -0,0 +1,214 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a charity dashboard",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 2,
+      title: "Fatima Taqavi Portfolio V1",
+      des: "A portfolio for a web designer.",
+      img: "/p1.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://noghtetalaei.ir/",
+    },
+    {
+      id: 1,
+      title: "Dr Saaki Clinic",
+      des: "A cosmetic surgeon clinic website.",
+      img: "/p4.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://drsakiclinic.com/",
+    },
+    {
+      id: 3,
+      title: "Movie Watchlist Application",
+      des: "A REAL Software-as-a-Service app with API features using the latest tech stack.",
+      img: "/p3.jpg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Fatimataqavi/movie-watchlist",
+    },
+    {
+      id: 4,
+      title: "Bonyad Salman Charity Dashboard",
+      des: "A dashboard for caregivers to be able to track and report the individuals under their supervision in a charity organization ..",
+      img: "/p2.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://app.bonyadsalman.ir",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Fatima Taqavi is an exceptional web developer! She exceeded my expectations by delivering a sleek and functional website for my business in record time. Her attention to detail and creative design skills truly set her apart. Highly recommend",
+      name: "Dr Saaki",
+      title: "Cosmetic surgeon",
+    },
+    {
+      quote:
+        "Working with Fatima was a pleasure from start to finish. Her professionalism, expertise, and responsiveness made the entire website development process smooth and stress-free. I now have a modern and user-friendly website thanks to her skills and dedication!",
+      name: "Mr. Aletaha",
+      title: "Head of Salman charity",
+    },
+    {
+      quote:
+        "Fatima Taqavi is a true gem in the world of web development. Her ability to understand my vision and turn it into a stunning reality online was nothing short of amazing. I am beyond impressed with her work ethic and technical proficiency. Choose Fatima for your next project, you won't be disappointed!",
+      name: "Mr Dastaviz",
+      title: "CEO of Shirazvand",
+    },
+    {
+      quote:
+        " Fatima is an absolute rockstar in the field of web development. She transformed my outdated website into a visually captivating and highly functional platform that truly reflects my brand. I am so grateful for her expertise and dedication to delivering quality results. Look no further for your web development needs - Fatima is the one!",
+      name: "Mr Bazleh",
+      title: "CEO of Noghtetalaei Art studio",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      title: "github"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      title: "twitter"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      title: "linkedin"
+    },
+  ];
